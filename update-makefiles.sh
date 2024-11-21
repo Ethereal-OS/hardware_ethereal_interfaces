07d43671a97e2fb3618b5fb18f2b8d2f8451c412
IamCOD3X <sourabh.panchal28@gmail.com>
2024-11-21 16:04:14 +0530

    ETHEREAL ADOPT

--- update-makefiles.sh
+++ update-makefiles.sh
@@ -3,7 +3,7 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.voltage:hardware/voltage/interfaces" \
+  "vendor.ethereal:hardware/ethereal/interfaces" \
   "android.hardware:hardware/interfaces" \
   "android.hidl:system/libhidl/transport"
 
